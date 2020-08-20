69af76bf6329d089099711c0afc6f07fb18fecae
Manos Kamarianakis <m.kamarianakis@gmail.com>
2020-08-20 10:12:25 +0300

    initial commit

---
+++ About_me_files/About_me.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20200720
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:5.83}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_me_files/About_meMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
